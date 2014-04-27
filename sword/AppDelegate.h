835b48226eb7503d5f139d5c9d495ca20b8aa41e
Verdi Ergun <vergun@gmail.com>
2014-04-27 09:04:53 -0700

    Initial Commit

---
+++ sword/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sword
+//
+//  Created by Verdi Ergun on 4/27/14.
+//  Copyright (c) 2014 Verdi Ergun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
